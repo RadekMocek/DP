26002a5264ca35a859ac4cda0caf421109d29df1
RadekMocek <radek.mocek@tul.cz>
2025-10-23 01:09:35 +0200

    Dear ImGui HelloCounter

--- 1source/00DearImGui/01HelloCounter/App/AppUpdate.cpp
+++ 1source/00DearImGui/01HelloCounter/App/AppUpdate.cpp
@@ -2,12 +2,38 @@
 
 void App::Update()
 {
-    static int counter = 0;
+    // State
+    static int counter = 1;
+    static bool show_popup_window = false;
 
-    ImGui::Begin("Hello Counter");
+    // Const
+    static const ImVec2 button_size(35, 0);
 
-    ImGui::BeginDisabled(counter == 0);
-    if (ImGui::Button("-")) {
+    // Main menu bar
+    ImGui::BeginMainMenuBar();
+    if (ImGui::BeginMenu("File")) {
+        if (ImGui::MenuItem("Pop-up", nullptr, show_popup_window)) {
+            show_popup_window = !show_popup_window;
+        }
+        if (ImGui::MenuItem("Exit", "Alt+F4")) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+        ImGui::EndMenu();
+    }
+    ImGui::EndMainMenuBar();    
+
+    // Fullscreen window setup
+    const ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBringToFrontOnFocus;
+    const ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    static bool show_main_window = true;
+
+    // Fullscreen window begin
+    ImGui::Begin("Hello Counter", &show_main_window, flags);
+
+    ImGui::BeginDisabled(counter == 1);
+    if (ImGui::Button("-", button_size)) {
         counter--;
     }
     ImGui::EndDisabled();
@@ -19,7 +45,7 @@ void App::Update()
     ImGui::SameLine();
 
     ImGui::BeginDisabled(counter == 5);
-    if (ImGui::Button("+")) {
+    if (ImGui::Button("+", button_size)) {
         counter++;
     }
     ImGui::EndDisabled();
@@ -29,4 +55,17 @@ void App::Update()
     }
 
     ImGui::End();
+
+    // Pop-up window
+    if (show_popup_window) {
+        ImGui::Begin("Pop-up window");
+
+        ImGui::Text("Not much you can do here...");
+
+        if (ImGui::Button("Close")) {
+            show_popup_window = false;
+        }
+        
+        ImGui::End();
+    }
 }
