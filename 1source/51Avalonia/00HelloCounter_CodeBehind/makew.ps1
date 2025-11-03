3925c7dca5bb94201abfab58828a6c3d7d249a8e
RadekMocek <radek.mocek@tul.cz>
2025-11-03 03:09:07 +0100

    avalonia::hellocounter::mvvm, tex::rešerše::init

---
+++ 1source/51Avalonia/00HelloCounter_CodeBehind/makew.ps1
@@ -0,0 +1,7 @@
+$PROJNAME = "AvaloniaApplication1"
+$PUBLISHDIR = ".\PUBLISHED"
+
+$paths = $PUBLISHDIR, ".\${PROJNAME}\bin\Release\net9.0\win-x64"
+Get-Item -LiteralPath $paths -ErrorAction SilentlyContinue | Remove-Item -Recurse
+
+dotnet publish .\$PROJNAME\$PROJNAME.csproj --runtime win-x64 --no-self-contained --output $PUBLISHDIR
---
+++ 1source/51Avalonia/01HelloCounter_MVVM/AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaApplication2.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty] private int _counter = 1;
+
+    [ObservableProperty] private bool _isButtonDecrEnabled = false;
+    [ObservableProperty] private bool _isButtonIncrEnabled = true;
+    [ObservableProperty] private bool _isLabelHelloVisible = false;
+
+    [RelayCommand]
+    private void MenuExitClicked()
+    {
+        Environment.Exit(0);
+    }
+
+    [RelayCommand]
+    private void ButtonDecrClicked()
+    {
+        Counter--;
+        CounterUpdated();
+    }
+
+    [RelayCommand]
+    private void ButtonIncrClicked()
+    {
+        Counter++;
+        CounterUpdated();
+    }
+
+    private void CounterUpdated()
+    {
+        IsButtonDecrEnabled = Counter != 1;
+        IsButtonIncrEnabled = Counter != 5;
+
+        IsLabelHelloVisible = Counter == 3;
+    }
+}
