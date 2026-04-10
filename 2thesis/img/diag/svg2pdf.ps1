f4a49a36b32ff17985d593fd012f230795064c93
RadekMocek <radek.mocek@tul.cz>
2026-04-10 02:56:06 +0200

    latex 18 19 20

--- 2thesis/img/diag/svg2pdf.ps1
+++ 2thesis/img/diag/svg2pdf.ps1
@@ -1,1 +1,8 @@
-& "C:\_Programy\Inkscape\bin\inkscape.exe" --export-type=pdf wireframe.svg
+$filenames = @(
+    "err_high"
+    "wireframe"
+)
+
+$filenames | ForEach-Object {
+    & "C:\_Programy\Inkscape\bin\inkscape.exe" --export-type=pdf "${_}.svg"
+}
