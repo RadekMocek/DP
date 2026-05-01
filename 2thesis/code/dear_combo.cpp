5d7effdb30fd47b38a2f6f6befccb2178c1f22ce
RadekMocek <radek.mocek@tul.cz>
2026-05-01 06:33:44 +0200

    Korektura 1

--- 2thesis/code/dear_combo.cpp
+++ 2thesis/code/dear_combo.cpp
@@ -5,7 +5,6 @@ constexpr std::array node_types = {
     ICON_FORMAT_TEXT_VARIANT" Text"
 };
 static int idx; // Index právě vybrané položky v rozbalovacím seznamu
-
 ImGui::PushItemWidth(160); // Nastavení šířky ovládacího prvku
 if (GUICombo("##ComboNodeType", node_types, idx))
     { /* Uživatel vybral jinou položku ... */ }
